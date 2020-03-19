3cb4ae4609ac954ae37e3c4ada564652c95c0fa4
matthew-sessions <matthewlsessions@gmail.com>
2020-03-19 10:41:29 -0700

    all up to thursday

--- projects/social/social.py
+++ projects/social/social.py
@@ -17,11 +17,14 @@ class SocialGraph:
         """
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
             print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -52,16 +55,39 @@ lenth += 1
             self.add_user(f'User: {i + 1}')
 
         rand_comb = []
+        n = 1
         for i in self.users:
             for a in range(i +1, self.last_id + 1):
                 rand_comb.append((i, a))
+
         random.shuffle(rand_comb)
         # Create friendships
 
         for i in range(num_users * avg_friendships // 2):
             friendship = rand_comb[i]
             self.add_friendship(friendship[0], friendship[1])
+            print(n)
+            n += 1
 
+    def populate_graph_linear(self, num_users, avg_friendships):
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+        # !!!! IMPLEMENT ME
+        for i in range(num_users):
+            self.add_user(f'User: {i + 1}')   
+        # Add users
+        total_friendships = 0
+        target_friendships = num_users * avg_friendships // 2
+        colis = 0
+
+        while total_friendships < target_friendships:
+            user_id = random.randint(1, num_users)     
+            freind_id = random.randint(1, num_users)
+            if self.add_friendship(user_id, freind_id) == True:
+                total_friendships += 1
+            else:
+                colis += 1
 
     def get_all_social_paths(self, user_id):
         """
@@ -74,45 +100,61 @@ lenth += 1
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
-        changin_arr = [user_id]
-        depth = 0
-        while changin_arr != []:
-            new_arr = []
-            for i in changin_arr:
-                if i not in visited:
-                    visited[i] = depth
-                    next_friends = self.friendships[i]
-                    for a in next_friends:
-                        new_arr.append(a)
-            depth += 1
-            changin_arr = new_arr
+        q = deque()
+        q.append([user_id])
+        while q.__len__() > 0:
+            path = q.popleft()
+            current_id = path[-1]
+            if current_id not in visited:
+                visited[current_id] = path
+
+                for friend_id in self.friendships[current_id]:
+                    path_copy = path.copy()
+                    path_copy.append(friend_id)
+                    q.append(path_copy)
+
+        
+        return visited
+
+    # def get_all_social_paths(self, user_id):
+    #     """
+    #     Takes a user's user_id as an argument
+
+    #     Returns a dictionary containing every user in that user's
+    #     extended network with the shortest friendship path between them.
+
+    #     The key is the friend's ID and the value is the path.
+    #     """
+    #     visited = {}  # Note that this is a dictionary, not a set
+    #     # !!!! IMPLEMENT ME
+    #     changin_arr = [user_id]
+    #     depth = 0
+    #     while changin_arr != []:
+    #         new_arr = []
+    #         for i in changin_arr:
+    #             if i not in visited:
+    #                 visited[i] = depth
+    #                 next_friends = self.friendships[i]
+    #                 for a in next_friends:
+    #                     new_arr.append(a)
+    #         depth += 1
+    #         changin_arr = new_arr
+
+        
+    #     return visited
+
+    def socialstats(self, user_id):
+        visted = self.get_all_social_paths(user_id)
+        visted_arr = [i for i in visted.values]
+        lenth = len(visted_arr)
+        avg = sum(visted_arr) / lenth
+        return lenth, avg
 
         
 
-        # q = deque()
-        # q.append([user_id])
-        # curs = q.__len__()
-        # while curs > 0:
-
-        #     end_of_q = q.pop()
-        #     index = end_of_q[-1]
-        #     lenth_of_q = len(end_of_q) - 1
-        #     #print(index)
-        #     if index not in visited:
-        #         visited[index] = lenth_of_q
-        #     for i in self.friendships[user_id]:
-                
-        #         temp_arr = end_of_q.copy()
-        #         temp_arr.append(i)
-        #         q.append(temp_arr)
-        #     curs = q.__len__()
-
-        return visited
-
-
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
+    sg.populate_graph_linear(10, 2)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
